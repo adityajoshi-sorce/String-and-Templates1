46f7b1531212225f0fbcc7f69ec9da5597f959a5
adityajoshi-sorce <joshiaditya0727@gmail.com>
2023-07-08 14:32:13 +0530

    Safe committed

---
+++ script.js
@@ -0,0 +1,16 @@
+const firstName = "Aditya";
+const birthYear = 1994;
+const job = "Web Develper";
+const year = 2022;
+
+
+const data = `I'm ${firstName}, a ${year - birthYear}  year old ${job}!`;
+console.log(data);
+
+
+console.log(`
+	Hello
+	what 
+	is 
+	your 
+	name?`)
